0f44733bce866cd23605d813edc899b234d5151d
proff001 <thommhole@gmail.com>
2021-03-24 21:15:16 +0100

    Changed structure and back from VSCode to Norma lVisual Studio

---
+++ Content/Tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Tiles" tilewidth="32" tileheight="32" tilecount="667" columns="23">
+ <image source="Tiles.png" width="736" height="928"/>
+</tileset>
