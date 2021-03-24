bd1a6eeca7804bd54eb711707d2a942ec8c37110
proff001 <thommhole@gmail.com>
2021-03-24 23:28:51 +0100

    Another days work of testing!

---
+++ Labyrintspill/Labyrintspill/Content/Tiles/Details.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Details" tilewidth="48" tileheight="48" tilecount="896" columns="16">
+ <image source="../Textures/Interiors_free_48x48.png" width="768" height="2688"/>
+</tileset>
